17e7f0c0100d6243a07976f46c6097c01de60477
Rohith Dara <rohith.dara2000@gmail.com>
2019-03-27 21:46:21 -0700

    Add files via upload

---
+++ LAB1/lab1.py
@@ -0,0 +1,11 @@
+#
+# This is a header block example for lab 1.
+#
+# You will need to supply the following information.
+#
+# Name:
+# Section:
+#
+
+print ("Hello, Rohith.")
+
---
+++ LAB1/lab1_test_cases.py
@@ -0,0 +1,29 @@
+#
+# Test cases example for lab 1.
+#
+# Name: Rohith Dara
+# Section: 01
+#
+
+import unittest      # import the module that supports writing unit tests
+
+# Define a class that will be used for these test cases.
+# This class will include testing functions.
+#
+# Much of this code should be viewed as boilerplate for now.
+#
+class TestsLab1(unittest.TestCase):
+   def test_expressions(self):         # Defines one testing function.
+      self.assertEqual(0 + 1, 1)
+      self.assertEqual(2 * 2, 4)
+      self.assertAlmostEqual(19/3.0, 6.33333333333333)
+      self.assertEqual(19//3, 6)
+      self.assertAlmostEqual(19/3.0, 6.333333333333333)
+      self.assertAlmostEqual(19.0/3.0, 6.33333333333333)
+      self.assertEqual(4*2+27//3+4, 21)
+      self.assertEqual(4*(2+27)//3+4, 42)
+
+
+# Run the unit tests.
+if __name__ == '__main__':
+   unittest.main()
